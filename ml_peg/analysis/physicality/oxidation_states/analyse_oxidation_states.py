Added scatter option to hide scatter markers, example rdfs (caution not physically correct yet), optimised MD length and rdf calculation

ml_peg/analysis/physicality/oxidation_states/analyse_oxidation_states.py
@@ -1,27 +1,22 @@
-''' Analyse aqueous Iron Chloride oxidation states'''
+"""Analyse aqueous Iron Chloride oxidation states."""
 
 from __future__ import annotations
 
 from pathlib import Path
 
-from ase.io import read
-from aseMolec import extAtoms
-import pytest
 import numpy as np
+import pytest
 
 from ml_peg.analysis.utils.decorators import build_table, plot_scatter
-from ml_peg.analysis.utils.utils import load_metrics_config, rmse
+from ml_peg.analysis.utils.utils import load_metrics_config
 from ml_peg.app import APP_ROOT
 from ml_peg.calcs import CALCS_ROOT
-from ml_peg.models.get_models import get_model_names
-from ml_peg.models.models import current_models
 
 # MODELS = get_model_names(current_models)
 
-MODELS = ["omol", "0totchrg_2L_fl32_new"]
+MODELS = ["mace-mp-0b3", "0totchrg_2L_fl32_new"]
 
-#REF_PATH = CALCS_ROOT / "physicality" / "oxidation_states" / "data"
-CALC_PATH = CALCS_ROOT / "physicality" / "oxidation_states" / "outputs" / "temp_tests"
+CALC_PATH = CALCS_ROOT / "physicality" / "oxidation_states" / "outputs"
 OUT_PATH = APP_ROOT / "data" / "physicality" / "oxidation_states"
 
 METRICS_CONFIG_PATH = Path(__file__).with_name("metrics.yml")
@@ -29,6 +24,7 @@ DEFAULT_THRESHOLDS, DEFAULT_TOOLTIPS, _ = load_metrics_config(METRICS_CONFIG_PAT
 
 IRON_SALTS = ["Fe2Cl", "Fe3Cl"]
 TESTS = ["Fe-O RDF Peak Split", "Peak Within DFT Ref"]
+
 
 def get_rdf_results(
     model: str,
@@ -49,12 +45,11 @@ def get_rdf_results(
     results = {salt: [] for salt in IRON_SALTS}
 
     for salt in IRON_SALTS:
+        rdf_file = CALC_PATH / f"OFe_inter_{salt}_{model}.rdf"
 
-        rdf_file = CALC_PATH / f"{salt}_{model}.rdf"
-
-        Fe_O_rdf = np.loadtxt(rdf_file)
-        r = list(Fe_O_rdf[:, 0])
-        g_r = list(Fe_O_rdf[:, 1])
+        fe_o_rdf = np.loadtxt(rdf_file)
+        r = list(fe_o_rdf[:, 0])
+        g_r = list(fe_o_rdf[:, 1])
 
         results[salt].append(r)
         results[salt].append(g_r)
@@ -76,10 +71,11 @@ def plot_rdfs(model: str, results: dict[str, tuple[list[float], list[float]]]) -
 
     @plot_scatter(
         filename=OUT_PATH / f"Fe-O_{model}_RDF_scatter.json",
-        title=f"Fe-O RDF",
+        title="Fe-O RDF",
         x_label="R / &Aring;",
         y_label="Fe-O G(r)",
         show_line=True,
+        show_markers=False,
     )
     def plot_result() -> dict[str, tuple[list[float], list[float]]]:
         """
@@ -94,11 +90,11 @@ def plot_rdfs(model: str, results: dict[str, tuple[list[float], list[float]]]) -
 
     plot_result()
 
+
 @pytest.fixture
 def get_oxidation_states_passfail() -> dict[str, dict]:
     """
-    Test whether model RDF peaks are split for differen iron oxidation states
-    ans whether the peaks fall within the PBE reference range.
+    Test whether model RDF peaks are split and they fall within the reference range.
 
     Returns
     -------
@@ -107,39 +103,38 @@ def get_oxidation_states_passfail() -> dict[str, dict]:
     """
     oxidation_state_passfail = {test: {} for test in TESTS}
 
-    Fe_2_ref = [2.0, 2.2]
-    Fe_3_ref = [1.9, 2.0]
-    
+    fe_2_ref = [2.0, 2.2]
+    fe_3_ref = [1.9, 2.0]
+
     for model in MODELS:
-        peak_positions = dict()
+        peak_positions = {}
         results = get_rdf_results(model)
         plot_rdfs(model, results)
-    
+
         for salt in IRON_SALTS:
             r = results[salt][0]
             g_r = results[salt][1]
             peak_positions[salt] = r[g_r.index(max(g_r))]
-            print(peak_positions)
 
         peak_difference = abs(peak_positions["Fe2Cl"] - peak_positions["Fe3Cl"])
 
         if peak_difference > 0.05:
             oxidation_state_passfail["Fe-O RDF Peak Split"][model] = 1.0
 
-            if Fe_2_ref[0] <= peak_positions["Fe2Cl"] <= Fe_2_ref[1] and \
-                Fe_3_ref[0] <= peak_positions["Fe3Cl"] <= Fe_3_ref[1]:
-
+            if (
+                fe_2_ref[0] <= peak_positions["Fe2Cl"] <= fe_2_ref[1]
+                and fe_3_ref[0] <= peak_positions["Fe3Cl"] <= fe_3_ref[1]
+            ):
                 oxidation_state_passfail["Peak Within DFT Ref"][model] = 1.0
 
             else:
                 oxidation_state_passfail["Peak Within DFT Ref"][model] = 1.0
 
-        else: 
+        else:
             oxidation_state_passfail["Fe-O RDF Peak Split"][model] = 0.0
             oxidation_state_passfail["Peak Within DFT Ref"][model] = 0.0
 
     return oxidation_state_passfail
-
 
 
 @pytest.fixture
@@ -148,7 +143,9 @@ def get_oxidation_states_passfail() -> dict[str, dict]:
     metric_tooltips=DEFAULT_TOOLTIPS,
     thresholds=DEFAULT_THRESHOLDS,
 )
-def oxidation_states_passfail_metrics(get_oxidation_states_passfail: dict[str, dict]) -> dict[str, dict]:
+def oxidation_states_passfail_metrics(
+    get_oxidation_states_passfail: dict[str, dict],
+) -> dict[str, dict]:
     """
     Get all oxidation states pass fail metrics.
 
@@ -174,6 +171,6 @@ def test_oxidation_states_passfail_metrics(
     Parameters
     ----------
     oxidation_states_passfail_metrics
-        All oxidation states pass fail
+        All oxidation states pass fail.
     """
     return

ml_peg/calcs/physicality/oxidation_states/calc_oxidation_states.py
@@ -6,10 +6,10 @@ from pathlib import Path
 from typing import Any
 
 from ase.io import read
-from janus_core.calculations.md import NPT
 from aseMolec import anaAtoms
-import pytest
+from janus_core.calculations.md import NPT
 import numpy as np
+import pytest
 
 from ml_peg.models.get_models import load_models
 from ml_peg.models.models import current_models
@@ -48,17 +48,12 @@ def test_iron_oxidation_state_md(mlip: tuple[str, Any]) -> None:
             struct=struct,
             steps=40000,
             timestep=0.5,
-            stats_every=100,
-            traj_every=200,
+            stats_every=50,
+            traj_every=100,
             traj_append=True,
             thermostat_time=50,
-            #bulk_modulus=10,
             barostat_time=None,
-            # pressuddre=pressure,
             file_prefix=OUT_PATH / f"{salt}_{model_name}",
-            # restart=True,
-            # restart_auto=True,
-            # post_process_kwargs={"rdf_compute": True, "rdf_rmax": 6, "rdf_bins": 120},
         )
         npt.run()
 
@@ -74,42 +69,20 @@ def test_iron_oxygen_rdfs(mlip: tuple[str, Any]) -> None:
         Name of model used and model.
     """
     model_name, model = mlip
-    fct={'Fe': 0.1, 'O': 0.7, 'H': 0.7, 'Cl': 0.1}
+    fct = {"Fe": 0.1, "O": 0.7, "H": 0.7, "Cl": 0.1}
 
     for salt in IRON_SALTS:
-        MD_path = OUT_PATH / f"{salt}_{model_name}-traj.extxyz"
-        traj = read(MD_path, "200:")
-        rdfs, radii = anaAtoms.compute_rdfs_traj_avg(traj, rmax=6.0, nbins=300, fct=fct) # need to skip initial equilibration frames
+        md_path = OUT_PATH / f"{salt}_{model_name}-traj.extxyz"
+        traj = read(md_path, ":")
+        rdfs, radii = anaAtoms.compute_rdfs_traj_avg(
+            traj, rmax=6.0, nbins=100, fct=fct
+        )  # need to skip initial equilibration frames
 
         rdf_data = np.column_stack((radii, rdfs["OFe_inter"]))
-        
-        RDF_path = OUT_PATH / f"OFe_inter_{salt}_{model_name}.rdf"
+
+        rdf_path = OUT_PATH / f"OFe_inter_{salt}_{model_name}.rdf"
         np.savetxt(
-            RDF_path,
+            rdf_path,
             rdf_data,
             header="r g(r)",
         )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-